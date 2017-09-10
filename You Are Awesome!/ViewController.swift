e66310709bf4da8f1468107e4ffe8bd9798f6745
Michelle Krameisen <michellekrameisen@michelles-macbook-pro.local>
2017-09-10 17:10:39 -0400

    video 2.15

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -17,13 +17,40 @@ class ViewController: UIViewController {
    var index = -1
    var imageNumber = -1
    var soundNumber = -1
+   var soundName = ""
    let numberOfImages = 4
    let numberOfSounds = 4
+    
     
     
     //this code executes when the view controller loads
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func playSound(soundName: String) {
+        // Can we load in the file soundName?
+        if let sound = NSDataAsset(name: soundName) {
+            // check if sound.data is a sound file
+            do {
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                awesomePlayer.play()
+            } catch {
+                // if sound.data is not a valid audio file
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        } else {
+            // if reading in the NSDataAsset didn't work, tell the user / report the error.
+            print("ERROR: file\(soundName) didn't load.")
+        }
+    }
+    
+    func nonRepeatingRandomNumber(lastNumber: Int, maxValue:Int) -> Int {
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
+        } while lastNumber == newIndex
+        return newIndex
     }
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
@@ -34,56 +61,32 @@ class ViewController: UIViewController {
                         "When the Genius Bar needs help, they call you!",
                         "You brighten my day!", "You Are Da Bomb!", "I can't wait to use your app!", "Fabulous? That's you!" ]
         
-        var newIndex = -1
-
-        // Show a message
-        repeat {
-            newIndex = Int(arc4random_uniform(UInt32(messages.count)))
-        } while index == newIndex
-        
-        index = newIndex
+        //Show a message
+        index = nonRepeatingRandomNumber(lastNumber: index, maxValue:messages.count)
         messageLabel.text = messages[index]
         
-        awesomeImage.isHidden = false //Show an image
-        repeat {
-            newIndex = Int(arc4random_uniform(UInt32(numberOfImages)))
-        } while imageNumber == newIndex
+        // Show an image
+        awesomeImage.isHidden = false //Show the image
         
-        
-        imageNumber = newIndex
+        imageNumber = nonRepeatingRandomNumber(lastNumber: imageNumber, maxValue: numberOfImages)
         awesomeImage.image = UIImage(named: "image\(imageNumber)")
         
         // Get a random number to use in our soundName file
-        repeat {
-            newIndex = Int(arc4random_uniform(UInt32(numberOfSounds)))
-        } while soundNumber == newIndex
-    
-        soundNumber = newIndex
+        soundNumber = nonRepeatingRandomNumber(lastNumber: soundNumber, maxValue: numberOfSounds)
         
         //Play a sound
-        var soundName = "sound\(soundNumber)"
-        
-        // Can we load in the file soundName?
-        if let sound = NSDataAsset(name: soundName) {
-            // check if sound.data is a sound file
-            do {
-            try awesomePlayer = AVAudioPlayer(data: sound.data)
-                awesomePlayer.play()
-            } catch {
-                // if sound.data is not a valid audio file
-                print("ERROR: data in \(soundName) couldn't be played as a sound.")
-            }
-            
-        } else {
-            // if reading in the NSDataAsset didn't work, tell the user / report the error.
-            print("ERROR: file\(soundName) didn't load.")
-        }
-      
-        
-        
+        let soundName = "sound\(soundNumber)"
+        playSound(soundName: soundName)
     }
         
 }
+
+
+
+
+
+
+
 //        var randomIndex =
 //            Int(arc4random_uniform(UInt32(messages.count)))
 //        messageLabel.text = messages[randomIndex]
